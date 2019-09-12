076183584004e6093475335e7705068cbecf0913
landreola <la48076@uga.edu>
2019-09-12 10:28:25 -0400

    OniomAtom subclass and test case

---
+++ oniomatoms.py
@@ -0,0 +1,140 @@
+import numpy as np
+from AaronTools.atoms import Atom
+from AaronTools.const import ATOM_TYPES, CONNECTIVITY, EIJ, ELEMENTS, MASS, RADII, RIJ, ATOM_TYPES
+
+class OniomAtom(Atom):
+    def __init__(self, element="", coords=[], flag=False, name="", tags=[], layer="", atomtype="", charge=""):
+        super().__init__(element="", coords=[], flag=False, name="", tags=[])
+
+        element = str(element).strip().capitalize()
+        if element == "":
+            self.element = element
+            self._radii = None
+            self._connectivity = None
+        elif element in ELEMENTS:
+            self.element = element
+            self._set_radii()
+            self._set_connectivity()
+        else:
+            raise ValueError("Unknown element detected:", element)
+
+        self.coords = np.array(coords, dtype=float)
+        self.flag = bool(flag)
+        self.name = str(name).strip()
+
+        if hasattr(tags, "__iter__") and not isinstance(tags, str):
+            self.tags = set(tags)
+        else:
+            self.tags = set([tags])
+
+        self.connected = set([])
+        self.constraint = set([])
+        self._rank = None
+        charge=str(charge).strip()
+        if charge == "":
+            pass
+        else:
+            self.charge = float(charge)
+
+        atomtype = str(atomtype).strip()
+        layer=str(layer).strip().capitalize()
+        if layer == "":
+            self.layer=layer
+        elif layer not in ['H', 'L', 'M']:
+            self.layer="H"
+            raise ValueError("Layer set to 'High.' Incorrect symbol for layer: " + layer)
+        else:
+            self.layer=layer
+
+        if atomtype == "":
+            self.atomtype=atomtype
+        elif atomtype not in ATOM_TYPES:
+            self.atomtype=atomtype
+            raise ValueError("Atom type " + self.atomtype + " not in reference")
+        else:
+            self.atomtype=atomtype
+
+    def __repr__(self):
+        s = ""
+        s += "{:>3s}  ".format(self.element)
+        for c in self.coords:
+            s += "{: 13.8f} ".format(c)
+        s += " {: 2d}  ".format(-1 if self.flag else 0)
+        s += "{}".format(self.name)
+        s += "   {}".format(self.layer)
+        s += "   {}".format(self.atomtype)
+        s += "   {}".format(self.charge)
+        return s
+
+    def get_layer(self):
+        if self.layer not in ['H', 'L', 'M']:
+            raise ValueError("Layer set to High. Incorrect symbol for layer: " + self.layer)
+        else:
+            return self.layer
+
+    def get_charge(self):
+        return self.charge
+
+    def get_at(self):
+        return self.atomtype
+        if self.atomtype not in ATOM_TYPES:
+            raise ValueError("Atom type " + self.atomtype + " not in reference")
+
+    def change_layer(self, newlayer):
+        self.layer = str(newlayer).strip().capitalize()
+ 
+    def change_atomtype(self, newtype):
+        self.atomtype = str(newtype).strip()
+ 
+    def change_charge(self, newcharge):
+        self.charge[0] = float(newcharge)
+
+class ChargeSum:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def get(cls, atomlist):
+        """sums charges in list of atoms of OniomAtom type."""
+        totalcharge=[]
+        if isinstance(atomlist, (list, tuple, np.ndarray)):
+            for atom in atomlist:
+                if isinstance(atom, OniomAtom):
+                    totalcharge=np.append(totalcharge, atom.charge)
+                else:
+                    raise TypeError("each atom in list must be an OniomAtom")
+            return np.sum(totalcharge)
+        else:
+            raise TypeError("function only accepts a list, tuple, or array of atoms")
+
+class OniomSanity:
+    def __init__(self):
+        pass
+    @classmethod
+    def check_charges(cls, atomlist, targetcharge, threshold=""):
+        """returns True if sum of charges is equal to expected value within threshold"""
+        if threshold == "":
+            threshold=float(10**(-1))
+        else:
+            threshold=float(threshold)
+        targetcharge = float(targetcharge)
+        return abs(ChargeSum.get(atomlist)-targetcharge) < threshold
+
+    @classmethod
+    def check_types(cls,atomlist):
+        """returns True if atomtypes in atomlist are all in reference"""
+        counter = 0
+        for atom in atomlist:
+            if atom.get_at():
+                counter+=1
+        return counter == len(atomlist)
+
+    @classmethod
+    def check_layers(cls, atomlist):
+        """returns True if layers are defined correctly as either H, L, or M"""
+        counter=0
+        for atom in atomlist:
+            if atom.get_layer():
+                counter+=1
+        return counter==len(atomlist)
+
---
+++ test/test_oniomatoms.py
@@ -0,0 +1,235 @@
+#! /usr/bin/env python3
+import unittest
+import random
+import numpy as np
+
+from AaronTools.atoms import Atom, BondOrder
+from AaronTools.geometry import Geometry
+from AaronTools.test import prefix, TestWithTimer
+from AaronTools.oniomatoms import OniomAtom, ChargeSum, OniomSanity
+
+class TestBondOrder(TestWithTimer):
+    small_mol = prefix + "test_files/malonate-3_waters.xyz"
+
+    def test_get(self):
+        mol = Geometry(TestBondOrder.small_mol)
+
+        bond_data = BondOrder()
+        for i, a in enumerate(mol.atoms):
+            for j, c in enumerate(mol.atoms):
+                if j != i:
+                    if c not in a.connected:
+                        continue
+                    bo = bond_data.get(a, c)
+                    if a.element == "C" and c.element == "C":
+                        # C-C bonds should all be single
+                        self.assertEqual(bo, 1)
+                    elif (a.element == "C" and c.element == "O" and "H" in c.connected) \
+                            or (c.element == "C" and a.element == "O" and "H" in a.connected):
+                        # C-OH bonds are single
+                        self.assertEqual(bo, 1)
+                    elif (a.element == "C" and c. element == "O" and "H" not in c.connected) \
+				            or (c.element == "C" and a.element == "O" and "H" not in a.connected):
+                        for k, d in enumerate(mol.atoms):
+                            if k != i and k != j:
+                                if (a.element =="C" and d in a.connected and d == "O") \
+                                      or (c.element == "C" and d in c.connected and d == "O"):
+                                    if "H" in d.connected:
+                                        # Carbonyl of a carboxylic acid is double
+                                        self.assertEqual(bo, 2)
+                                    else:
+                                        # Carbonyl of a carboxylate is 1.5
+                                        self.assertEqual(bo, 1.5)
+                    else:
+                        # all other bonds are single
+                        self.assertEqual(bo, 1)
+        return
+
+
+class TestAtoms(TestWithTimer):
+    # Test constants
+    small_mol = prefix + "test_files/malonate-3_waters.xyz"
+
+    # helper functions
+    def read_xyz(self, fname):
+        mol = []
+        with open(fname) as f:
+            # split xyz file into info matrix
+            tmp = f.read()
+            tmp = tmp.split("\n")
+            for line in tmp:
+                line = line.strip().split()
+                if len(line):
+                    mol += [line]
+            # remove atom number and comment line
+            mol = mol[2:]
+        atoms = []
+        for i, a in enumerate(mol):
+            a = OniomAtom(element=a[0], coords=a[1:4], name=(i + 1), tags=i, layer=a[4], atomtype=a[5], charge=a[6])
+            atoms += [a]
+        return atoms
+
+    # test cases
+    def test_store_atoms(self):
+        atoms_read = self.read_xyz(TestAtoms.small_mol)
+        atoms_manual = []
+        mol = []
+        with open(TestAtoms.small_mol) as f:
+            # split xyz file into info matrix
+            tmp = f.read()
+            tmp = tmp.split("\n")
+            for line in tmp:
+                line = line.strip().split()
+                if len(line):
+                    mol += [line]
+            # remove atom number and comment line
+            mol = mol[2:]
+        for i, line in enumerate(mol):
+            a = OniomAtom()
+            a.element = line[0]
+            a.coords = np.array(line[1:4], dtype=float)
+            a.flag = False
+            a.name = str(i + 1)
+            a.tags = set([i])
+            a.layer = line[4]
+            a.atomtype = line[5]
+            a.charge = line[6]
+            atoms_manual += [a]
+        # test manual and **kwargs assignment match
+        for a, b in zip(atoms_read, atoms_manual):
+            self.assertEqual(a.element, b.element)
+            self.assertTrue(np.linalg.norm(a.coords - b.coords) < 10**(-8))
+            self.assertEqual(a.name, b.name)
+            self.assertEqual(a.tags, b.tags)
+            self.assertEqual(a.layer, b.layer)
+            # should have automatic lookup of _radii
+            self.assertTrue(a._radii is not None)
+        # transform back into xyz format for verification
+        atom_matrix = []
+        for a in atoms_read:
+            tmp = [str(a.element)]
+            for c in a.coords:
+                tmp += ["{}".format(c)]
+            tmp += [str(a.layer)]
+            tmp += [str(a.atomtype)]
+            tmp += [str(a.charge)]
+            atom_matrix += [tmp]
+        for a, b in zip(mol, atom_matrix):
+            a = ' '.join(a)
+            b = ' '.join(b)
+            self.assertEqual(a, b)
+
+        return
+
+    # utilities
+    def test_float(self):
+        atom_args = [
+            {'element': 'H', 'coords': [0, 0, 0], 'name': '1'},
+            {'element': 'H', 'coords': [0, 0, 0], 'name': '1.1'},
+            {'element': 'H', 'coords': [0, 0, 0], 'name': '1.1.1'}
+        ]
+        atom_floats = [1., 1.1, 1.1]
+        for kwarg, ref in zip(atom_args, atom_floats):
+            atom = OniomAtom(**kwarg)
+            self.assertTrue(float(atom) == ref)
+
+    def test_less_than(self):
+        atoms = Geometry(TestAtoms.small_mol)
+        atoms = atoms.atoms
+        self.assertTrue(atoms[2] < atoms[3])
+        self.assertTrue(atoms[4] < atoms[2])
+        self.assertTrue(atoms[5] < atoms[3])
+
+    def test_add_tag(self):
+        mol = self.read_xyz(TestAtoms.small_mol)
+        for atom in mol:
+            new_tags = [random.random() for i in range(10)]
+            old_tags = atom.tags
+            atom.add_tag(new_tags[0])
+            atom.add_tag(new_tags[1], new_tags[2], new_tags[3], new_tags[4])
+            atom.add_tag(new_tags[5:])
+            self.assertSequenceEqual(
+                sorted(old_tags.union(new_tags)), sorted(atom.tags))
+
+    def test_get_invariant(self):
+        pentane = Geometry(prefix + "test_files/pentane.xyz")
+        mol = Geometry(prefix + "test_files/6a2e5am1hex.xyz")
+
+        s = ''
+        for a in pentane.atoms:
+            if a.element == 'H':
+                continue
+            s += a.get_invariant() + " "
+        self.assertEqual(s, "1010063 2020062 2020062 2020062 1010063 ")
+
+        s = ''
+        for a in mol.atoms:
+            if a.element == 'H':
+                continue
+            s += a.get_invariant() + " "
+        ans = "3030061 2020062 2020062 3030061 2020062 2020062 2020062 2020062 1010081 1010063 1010072 1010072 "
+        self.assertEqual(s, ans)
+
+    # measurement
+
+    def test_is_connected(self):
+        atoms = self.read_xyz(TestAtoms.small_mol)
+        conn_valid = ['2,3,4',
+                      '1',
+                      '1,5,8,9',
+                      '1,10',
+                      '3,6,7',
+                      '5',
+                      '5',
+                      '3',
+                      '3',
+                      '4',
+                      '12',
+                      '11,13',
+                      '12',
+                      '15',
+                      '14,16',
+                      '15',
+                      '18',
+                      '17,19',
+                      '18']
+        for i, a in enumerate(atoms):
+            for j, b in enumerate(atoms[i+1:]):
+                if a.is_connected(b):
+                    a.connected.add(b)
+                    b.connected.add(a)
+            conn_try = sorted([int(c.name) for c in a.connected])
+            conn_try = ','.join([str(c) for c in conn_try])
+            self.assertEqual(conn_try, conn_valid[i])
+        return
+
+    def test_bond(self):
+        a1 = Atom(element='H', coords=[0, 0, 0])
+        a2 = Atom(element='H', coords=[1, 0, 0])
+        self.assertTrue(np.linalg.norm(a1.bond(a2) - [1, 0, 0]) == 0)
+
+    def test_dist(self):
+        a1 = Atom(element='H', coords=[0, 0, 0])
+        a2 = Atom(element='H', coords=[1, 0, 0])
+        self.assertTrue(a1.dist(a2) == 1)
+
+    def test_angle(self):
+        mol = self.read_xyz(TestAtoms.small_mol)
+        angle = mol[17].angle(mol[16], mol[18])
+        self.assertTrue(abs(np.rad2deg(angle) - 101.1) < 10**(-1))
+
+class TestOniomSanity(TestWithTimer):
+    a = TestAtoms()
+    atomlist = a.read_xyz(TestAtoms.small_mol)
+ 
+    def test_check_charges(self):
+        self.assertTrue(OniomSanity.check_charges(self.atomlist[:],-1))
+
+    def test_check_types(self):
+        self.assertTrue(OniomSanity.check_types(self.atomlist[:]))
+
+    def test_check_layers(self):
+        self.assertTrue(OniomSanity.check_layers(self.atomlist))
+
+if __name__ == "__main__":
+    unittest.main()
