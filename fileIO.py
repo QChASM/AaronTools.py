5f986ac77164eff1eff9017be70283bf2ddca880
landreola <la48076@uga.edu>
2022-08-05 18:23:09 -0400

    fixed substitute cls bug for specifying inpute file format

--- fileIO.py
+++ fileIO.py
@@ -973,7 +973,20 @@ class FileReader:
                 max_length=max_length,
             )
         elif isinstance(self.content, str):
-            f = StringIO(self.content)
+            #f = StringIO(self.content)
+            if os.path.isfile(self.name):
+                f = open(self.name, "r", encoding="utf8")
+            else:
+                fname = ".".join([self.name, self.file_type])
+                fname = os.path.expanduser(fname)
+                if os.path.isfile(fname):
+                    f = open(fname, "r", encoding="utf8")
+                else:
+                    raise FileNotFoundError(
+                        "Error while looking for %s: could not find %s or %s in %s"
+                        % (self.name, fname, self.name, os.getcwd())
+                    )
+
         elif isinstance(self.content, IOBase):
             f = self.content
 
