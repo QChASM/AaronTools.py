c59b1b771bb8ac76f5337100895d8894a75aae70
Tony Schaefer <normnite@gmail.com>
2025-10-15 02:04:44 -0600

    reading more SAPT data

--- fileIO.py
+++ fileIO.py
@@ -2085,7 +2085,10 @@ class FileReader:
                     if line.strip().startswith("Total Energy ="):
                         self.other["energy"] = float(line.split()[-1])
                         self.other["energy_context"] = line
-    
+                        if "SAPT step" in self.other:
+                            item = "%s %s" % (self.other["SAPT step"], "energy")
+                            self.other[item] = self.other["energy"]
+ 
                     elif line.strip().startswith("Total E0"):
                         self.other["energy"] = float(line.split()[-2])
                         self.other["energy_context"] = line
@@ -2256,6 +2259,17 @@ class FileReader:
     
                         self.other["forces"] = -gradient
     
+                    elif "//" in line and ("Dimer" in line or "Monomer" in line):
+                        self.other["SAPT step"] = line.strip().strip("//").strip()
+    
+                    elif ("(A<-B)" in line or "(B<-A)" in line) and "=" in line:
+                        # charge transfer SAPT results, e.g.
+                        #     Exch-Ind20,r (A<-B) =     number [Eh]
+                        item = line.split("=")[0].strip()
+                        if "SAPT step" in self.other:
+                            item = "%s %s" % (self.other["SAPT step"], item)
+                        self.other[item] = float(line.split()[-2].strip())
+    
                     elif "SAPT Results" in line:
                         self.skip_lines(f, 1)
                         n += 1
@@ -2269,6 +2283,8 @@ class FileReader:
                                     continue
                                 item = line[:26].strip()
                                 val = 1e-3 * float(line[34:47])
+                                if "SAPT step" in self.other:
+                                    item = "%s %s" % (self.other["SAPT step"], item)
                                 self.other[item] = val
     
                     elif "SCF energy" in line:
